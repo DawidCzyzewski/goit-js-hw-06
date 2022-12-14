fe86c33422d3d89ad9eecf7424f6733022284e0b
Dawid Czyzewski <vinciebr@proton.me>
2022-12-14 21:41:28 +0100

    Dodano sprawdzanie białych znaków
    Poprawiono sprawdzanie, czy użytkownik nie wpisał białych znaków, co zamienia słowo Anonymous na puste pole

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,9 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 let handleData = () => {
-  if (event.target.value) {
-    nameOutput.textContent = nameInput.value;
+  if (event.target.value.trim() != "") {
+    //Tu przez mentora zalecony trim oraz sprawdzenie, czy nie ma pustych znaków. To usuwa spację np po słowie wprowadzonym, żeby wykrzynik nie był przesunięty i puste pole się nie wyświetlało
+    nameOutput.textContent = nameInput.value.trim(); //Tu tównież dodany trim
   } else {
     nameOutput.textContent = "Anonymous";
   }
